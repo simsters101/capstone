c3695eedb025ec53520893a19bcdf20220e87b36
Sim <simchasitzer1@gmail.com>
2019-07-31 22:32:10 -0400

    seed 614 destinations into the db using a JSON from the web

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# require 'http'
+
+# response = HTTP.get("https://pkgstore.datahub.io/core/airport-codes/airport-codes_json/data/7fd9ada72fd1c808e73f0f329428d094/airport-codes_json.json")
+
+# response = response.parse
+
+# large_airports = []
+
+# response.map do |destination|
+#   if destination["type"] == "large_airport"
+#     large_airports << destination
+#   end
+# end
+
+# large_airports.map do |airport|
+#   name = airport["municipality"]
+#   continent = airport["continent"]
+#   airport_code = airport["iata_code"]
+#   country = airport["iso_country"]
+#   Destination.create(name: name, continent: continent, airport_code: airport_code, country: country)
+# end
